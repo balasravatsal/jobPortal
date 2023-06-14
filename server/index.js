2d8c14f97406f26f95ce2af13bfb6300899b70ff
balasravatsal <vatsalbalasra24@gmail.com>
2023-06-14 10:13:50 +0530

    Directory changed

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('welcome')
+})
+
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Server listening on ${port}`)
+})
