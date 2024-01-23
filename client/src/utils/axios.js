3f5f431f1a39be6ca4a603b002899a5713a265ed
Vatsal <vatsalbalasra24@gmail.com>
2024-01-23 18:07:15 +0530

    ..

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,9 +1,11 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const customFetch = axios.create({
-    baseURL: process.env.NODE_ENV === "production"
-    ? "api/v1/"
-    :'http://localhost:5000/api/v1/'
+    baseURL: 
+        // process.env.NODE_ENV === "production" ?
+        "http://13.232.91.56/api/v1/"
+        // "http://ec2-13-200-215-25.ap-south-1.compute.amazonaws.com/api/v1/"
+        // : 'http://localhost:5000/api/v1/',
 })
 
-export default customFetch
+export default customFetch;
