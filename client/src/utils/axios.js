a4967668db74658b761c217a3fe5d911cf362366
Vatsal <vatsalbalasra24@gmail.com>
2024-01-24 21:45:32 +0530

    front-end ready to deploy

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -3,9 +3,7 @@ import axios from 'axios';
 const customFetch = axios.create({
     baseURL: 
         // process.env.NODE_ENV === "production" ?
-        "http://13.232.91.56/api/v1/"
-        // "http://ec2-13-200-215-25.ap-south-1.compute.amazonaws.com/api/v1/"
-        // : 'http://localhost:5000/api/v1/',
+        "http://13.233.233.194:5001/api/v1/"
 })
 
 export default customFetch;
